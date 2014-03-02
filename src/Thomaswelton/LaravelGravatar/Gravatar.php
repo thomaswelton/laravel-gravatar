76bb7b9102cc5afd843c6b2834ce0672c2595b4d
Thomas Welton <thomaswelton@me.com>
2014-03-02 19:00:49 +0000

    Class name is upperclass

--- src/Thomaswelton/LaravelGravatar/Gravatar.php
+++ src/Thomaswelton/LaravelGravatar/Gravatar.php
@@ -3,7 +3,7 @@
 use \Config;
 use Illuminate\Support\Facades\HTML;
 
-class Gravatar extends \thomaswelton\gravatarlib\Gravatar
+class Gravatar extends \thomaswelton\GravatarLib\Gravatar
 {
     private $default_size = null;
 
